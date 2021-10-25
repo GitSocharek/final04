86fac321a96aa9485c3bd4c5070ffb52eb05bb25
mic_boomer <micro.boomer@gmail.com>
2021-10-25 13:01:41 +0200

    str format

---
+++ tmp.py
@@ -0,0 +1,37 @@
+### 🔴 Ćwiczenie M04L05
+
+# Rozwiń program z poprzedniej lekcji tak, aby wyniki wyświetlić w tabeli. Użyj string interpolation. Dodaj nagłówek tabeli.
+
+import sys
+
+files = sys.argv
+LINES = 'LINES'
+WORDS = 'WORDS'
+CHARS = 'CHARS'
+FILENAME = 'FILENAME'
+# print(files)
+
+if len(files) == 1:
+    print('Missing filename(s).')
+    sys.exit(1)
+
+filepaths = files[1: ]
+
+print(f"{LINES:6} {WORDS:6} {CHARS:6} {FILENAME}")
+
+# print(filepaths)
+for file in filepaths:
+    with open(file, encoding = 'utf-8') as stream:
+        content = stream.read()
+
+    lines = len(content.split('\n'))
+    words = len(content.split())
+    char = len(content) - content.count('\n')
+    print(f"{lines:5} {words:6} {char:6} {file:^18}")
+print() 
+
+
+# left_alignment = "Left Text"
+# center_alignment = "Centered Text"
+# right_alignment = "Right Tex"
+# print(f"{left_alignment : <20}{center_alignment : ^15}{right_alignment : >20}")
