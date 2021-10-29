3dde9fa5465823a277365bcbf6313ac97f462d0a
mic_boomer <micro.boomer@gmail.com>
2021-10-29 13:18:52 +0200

    words counter - OK

--- final04.py
+++ final04.py
@@ -134,3 +134,25 @@
 # 2. Wyświetl w tabeli ile razy występuje każde ze słów.
 # 3. Nie zwracaj uwagi na wielkość liter w słowach, to znaczy "A" oraz "a" to jest to samo słowo. 
 # 4. W jaki jeszcze sposób przetworzył(a)byś tekst zanim podzielisz go na słowa?
+
+# import sys
+# text = sys.argv
+
+# PUNCTUATIONS = '.,?!'
+
+# for txt in text[1: ]:
+#     for punc in PUNCTUATIONS:
+#         txt = txt.replace(punc, '').lower()
+#     words = txt.split()
+  
+# dict = {}
+# for word in words:
+#     if word in dict:
+#         dict[word] += 1
+#     else:
+#         dict[word] = 1
+
+# for key_ in dict:
+#     print(f"{dict.get(key_, 0):3}", key_)
+
+### 🔴 Ćwiczenie M04L08
--- tmp.py
+++ tmp.py
@@ -1,23 +1,32 @@
-### 🔴 Ćwiczenie M04L06
+### 🔴 Ćwiczenie M04L07
 
-# Narodowy Bank Polski udostępnia przez swoje API historyczne kursy walut. Otrzymałæś odpowiedź taką jak poniżej
+# 1. Mając podany tekst zlicz poszczególne słowa.
+# 2. Wyświetl w tabeli ile razy występuje każde ze słów.
+# 3. Nie zwracaj uwagi na wielkość liter w słowach, to znaczy "A" oraz "a" to jest to samo słowo. 
+# 4. W jaki jeszcze sposób przetworzył(a)byś tekst zanim podzielisz go na słowa?
 
-response = {
-    "table": "A",
-    "currency": "dolar amerykański",
-    "code": "USD",
-    "rates": [
-        {
-            "no": "148/A/NBP/2021",
-            "effectiveDate": "2021-08-03",
-            "mid": 3.8315,
-        },
-    ],
-}
+import sys
 
-rate = response['rates']
-mid_currency = rate[0]
-mid = mid_currency['mid']
+text = sys.argv
+PUNCTUATIONS = '.,?!'
 
-print(mid)
+print(text) # tmp
 
+for txt in text[1: ]:
+    for punc in PUNCTUATIONS:
+        txt = txt.replace(punc, '').lower()
+    split_text = txt.split()
+print(split_text) # tmp
+  
+dict = {}
+for word in split_text:
+    if word in dict:
+        dict[word] += 1
+    else:
+        dict[word] = 1
+
+print(dict) # tmp
+
+for key in dict:
+    # print(f"{dict.get(i, 0), i:3}")
+    print(f"{dict.get(key, 0):3}", key)
